c8dcce39182e04522a68e209c539d8a0cbdbcd28
liqurH <1053183229@qq.com>
2022-09-08 10:37:45 +0800

    改变setSearchparam的实现方式

--- src/screens/project-list/util.ts
+++ src/screens/project-list/util.ts
@@ -1,51 +1,58 @@
-import { useMemo, useState } from "react"
-import { useProject } from "utils/project"
-import { useUrlQueryParam } from "utils/url"
+import { useMemo, useState } from "react";
+import { useProject } from "utils/project";
+import { useUrlQueryParam ,useSetUrlSearchParam} from "utils/url";
 
 // 项目类表搜索的参数
 export const useProjectsSearchParam = () => {
-    const [param ,setParam] = useUrlQueryParam(['name','personId'])  
-    // param的personId是string类型的，需要一个number类型的
-    return [
-        useMemo(() => ({...param , personId:Number(param.personId) || undefined}) , [param]) ,
-        setParam 
-    ] as const
-}
+  const [param, setParam] = useUrlQueryParam(["name", "personId"]);
+  // param的personId是string类型的，需要一个number类型的
+  return [
+    useMemo(
+      () => ({ ...param, personId: Number(param.personId) || undefined }),
+      [param]
+    ),
+    setParam,
+  ] as const;
+};
 
 export const useProjectQueryKey = () => {
-    const [params] = useProjectsSearchParam()
-    return ['projects' , params]
-}
-
+  const [params] = useProjectsSearchParam();
+  return ["projects", params];
+};
 
 // 用url管理项目模式状态框的hook  全局状态管理的功能
 export const useProjectModal = () => {
-    const [{projectCreate} , setProjectCreate] = useUrlQueryParam([
-        'projectCreate'
-    ])
-    const [{editingProjectId} ,setEditingProjectId] = useUrlQueryParam([
-        'editingProjectId'
-    ])
-    const {data:editingProject ,isLoading} = useProject(Number(editingProjectId))
+  const [{ projectCreate }, setProjectCreate] = useUrlQueryParam([
+    "projectCreate",
+  ]);
+  const [{ editingProjectId }, setEditingProjectId] = useUrlQueryParam([
+    "editingProjectId",
+  ]);
 
-    const open = () => setProjectCreate({projectCreate:true})  //项目模式状态框打开函数
-    const close = () => {
-        projectCreate 
-        ?setProjectCreate({projectCreate:undefined})
-        : setEditingProjectId({editingProjectId:undefined})
-    }  //项目模式状态框关闭函数
-    const startEdit = (id:number) => setEditingProjectId({editingProjectId:id}) // 编辑id项目
+  const { data: editingProject, isLoading } = useProject(
+    Number(editingProjectId)
+  );
 
-    return {
-        projectModalOpen: projectCreate === 'true' || Boolean(editingProject),
-        open,
-        close,
-        startEdit,
-        editingProject,
-        isLoading
-    }
-    
-}
+  const open = () => setProjectCreate({ projectCreate: true }); //项目模式状态框打开函数
+//   const close = () => {
+//     projectCreate
+//       ? setProjectCreate({ projectCreate: undefined })
+//       : setEditingProjectId({ editingProjectId: undefined });
+//   }; //项目模式状态框关闭函数
+  const setUrlParams = useSetUrlSearchParam();
+  const close = () => setUrlParams({ projectCreate: "", editingProjectId: "" });
+  const startEdit = (id: number) =>
+    setEditingProjectId({ editingProjectId: id }); // 编辑id项目
+
+  return {
+    projectModalOpen: projectCreate === "true" || Boolean(editingProject),
+    open,
+    close,
+    startEdit,
+    editingProject,
+    isLoading,
+  };
+};
 
 /*
     return [
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,32 +1,32 @@
-import { useMemo } from "react";
+import { useMemo ,useState} from "react";
 import { useSearchParams ,URLSearchParamsInit} from "react-router-dom";
-import { cleanObject } from "utils";
+import { cleanObject ,subset } from "utils";
 // searchParams获取param参数  https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
 // 根据url得param参数返回相应的页面
-export const useUrlQueryParam = <K extends string>(keys: K[]) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  return [
-    // 把“创建”函数和依赖项数组作为参数传入 useMemo，它仅会在某个依赖项改变时才重新计算 memoized 值
-    useMemo(
-      () =>
-        keys.reduce((prev, key) => {
-          // console.log('key',key);
-          // console.log('prev',prev);
-          // console.log('searchParams.get(key)',searchParams.get(key));  
-          return { ...prev, [key]: searchParams.get(key) || "" };
-        }, {} as { [key in K]: string }),
-        //state 的值，不会造成无限循环   
-      [searchParams, keys]
-    ),
-    (params:Partial<{[key in K ] : unknown}>) => {
-      // console.log('searchParams', searchParams); //searchParams URLSearchParams {}[[Prototype]]: URLSearchParams
-      // console.log('fromEntries-searchParams', Object.fromEntries(searchParams)); //fromEntries-searchParams {name: "kaui'di"}
-      const o = cleanObject({...Object.fromEntries(searchParams) , ...params}) as URLSearchParamsInit
-      return setSearchParams(o)
-    }
-  ] as const;
-  //  as const
-};
+// export const useUrlQueryParam = <K extends string>(keys: K[]) => {
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   return [
+//     // 把“创建”函数和依赖项数组作为参数传入 useMemo，它仅会在某个依赖项改变时才重新计算 memoized 值
+//     useMemo(
+//       () =>
+//         keys.reduce((prev, key) => {
+//           // console.log('key',key);
+//           // console.log('prev',prev);
+//           // console.log('searchParams.get(key)',searchParams.get(key));  
+//           return { ...prev, [key]: searchParams.get(key) || "" };
+//         }, {} as { [key in K]: string }),
+//         //state 的值，不会造成无限循环   
+//       [searchParams, keys]
+//     ),
+//     (params:Partial<{[key in K ] : unknown}>) => {
+//       // console.log('searchParams', searchParams); //searchParams URLSearchParams {}[[Prototype]]: URLSearchParams
+//       // console.log('fromEntries-searchParams', Object.fromEntries(searchParams)); //fromEntries-searchParams {name: "kaui'di"}
+//       const o = cleanObject({...Object.fromEntries(searchParams) , ...params}) as URLSearchParamsInit
+//       return setSearchParams(o)
+//     }
+//   ] as const;
+//   //  as const
+// };
 
 // as const Ts解析数组时希望其中的类型都一致
 /* 
@@ -66,3 +66,38 @@ export const useUrlQueryParam = <K extends string>(keys: K[]) => {
 */
 
 // Object.fromEntries(iterator)   ==> 把键值对列表转换为一个对象
+
+export const useUrlQueryParam = <K extends string>(keys: K[]) => {
+  const [searchParams] = useSearchParams();
+  const setSearchParams = useSetUrlSearchParam();
+  const [stateKeys] = useState(keys);
+  return [
+    useMemo(
+      () =>
+        subset(Object.fromEntries(searchParams), stateKeys) as {
+          [key in K]: string;
+        },
+      [searchParams, stateKeys]
+    ),
+    (params: Partial<{ [key in K]: unknown }>) => {
+      return setSearchParams(params);
+      // iterator
+      // iterator: https://codesandbox.io/s/upbeat-wood-bum3j?file=/src/index.js
+    },
+  ] as const;
+};
+
+export const useSetUrlSearchParam = () => {
+  const [searchParams, setSearchParam] = useSearchParams();
+  // console.log(searchParams);
+  // console.log(Object.fromEntries(searchParams));
+  
+  return (params: { [key in string]: unknown }) => {
+    const o = cleanObject({
+      ...Object.fromEntries(searchParams),
+      ...params,
+    }) as URLSearchParamsInit;
+    // console.log(o);
+    return setSearchParam(o);
+  };
+};
